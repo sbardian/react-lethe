e365e609eaebef6c2f16c2cacbd504535e099117
Brian Andrews <sbardian@gmail.com>
2020-08-14 21:51:20 -0600

    refactor: add login page

---
+++ src/pages/login-page.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { css } from '@emotion/core'
+
+const LoginPage = () => {
+  return (
+    <div
+      css={css`
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+      `}
+    >
+      <div></div>
+      <div>test</div>
+      <div></div>
+    </div>
+  )
+}
+
+export default LoginPage
