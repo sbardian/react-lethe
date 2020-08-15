2af4646963cabf30b39a211f6bf5970bd95af3a6
Brian Andrews <sbardian@gmail.com>
2020-08-15 11:54:47 -0600

    refactor: add toggle menu function

--- src/components/menu-button.js
+++ src/components/menu-button.js
@@ -1,6 +1,5 @@
 /** @jsx jsx */
 import React from 'react'
-import PropTypes from 'prop-types'
 import { jsx, css } from '@emotion/core'
 import { AiOutlineMenuFold, AiOutlineMenuUnfold } from 'react-icons/ai'
 import { MenuContext } from './menu-context'
@@ -9,22 +8,17 @@ const MenuButton = () => {
   const { isOpen, setIsOpen } = React.useContext(MenuContext)
   console.log('isOpen: ', isOpen)
 
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen)
+  }
+
   return isOpen ? (
     <AiOutlineMenuFold
       size="30"
       css={css`
         padding: 10px;
       `}
-      onClick={() => {
-        console.log('close')
-        //   const pageLayoutContainer = document.querySelector(
-        //     '.pagelayout-container',
-        //   )
-        //   const sidebar = document.querySelector('.sidebar')
-        //   pageLayoutContainer.style.gridTemplateColumns = '1fr'
-        //   sidebar.style.display = 'none'
-        setIsOpen(!isOpen)
-      }}
+      onClick={() => toggleSidebar()}
     />
   ) : (
     <AiOutlineMenuUnfold
@@ -32,16 +26,7 @@ const MenuButton = () => {
       css={css`
         padding: 10px;
       `}
-      onClick={() => {
-        console.log('open')
-        //   const pageLayoutContainer = document.querySelector(
-        //     '.pagelayout-container',
-        //   )
-        //   const sidebar = document.querySelector('.sidebar')
-        //   pageLayoutContainer.style.gridTemplateColumns = '1fr 4fr'
-        //   sidebar.style.display = 'grid'
-        setIsOpen(!isOpen)
-      }}
+      onClick={() => toggleSidebar()}
     />
   )
 }
