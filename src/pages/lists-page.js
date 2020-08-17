3be44a558f5ec1cb98f884301aaf9dfd4d6e9644
Brian Andrews <sbardian@gmail.com>
2020-08-16 22:24:28 -0600

    feat: add add list button

--- src/pages/lists-page.js
+++ src/pages/lists-page.js
@@ -2,6 +2,7 @@
 /** @jsx jsx */
 import React from 'react'
 import { jsx, css } from '@emotion/core'
+import { RiPlayListAddLine } from 'react-icons/ri'
 import PageLayout from '../components/page-layout'
 import Lists from '../components/lists'
 
@@ -13,9 +14,19 @@ const ListsPage = () => {
           color: #666;
           padding: 20px;
           display: grid;
-          grid-template-columns: 1fr;
+          grid-template-rows: 40px 1fr;
         `}
       >
+        <div
+          css={css`
+            justify-self: end;
+            color: #4ababa;
+            cursor: pointer;
+          `}
+          onClick={() => console.log('add list')}
+        >
+          <RiPlayListAddLine size="34" />
+        </div>
         <Lists />
       </div>
     </PageLayout>
