390a529c0f395fa01d417011a87131b577176afd
Brian Andrews <sbardian@gmail.com>
2020-08-25 10:48:32 -0600

    refactor: add context and theme decorators

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,21 @@
+import React from 'react'
+import { ThemeProvider } from 'theme-ui'
+import { AlertProvider } from 'react-alerts-plus'
+import { MenuProvider } from '../src/components/contexts/menu-context/menu-context'
+import theme from '../src/theme'
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
 }
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <MenuProvider>
+        <AlertProvider>
+          <Story />
+        </AlertProvider>
+      </MenuProvider>
+    </ThemeProvider>
+  ),
+]
