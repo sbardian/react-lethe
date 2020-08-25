93b0bf0f440b1fe29aeb0a537844e5c01f744600
Brian Andrews <sbardian@gmail.com>
2020-08-25 14:16:06 -0600

    test: add update list title button stories

---
+++ src/components/buttons/update-list-title-button/update-list-title-button.stories.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import { action } from '@storybook/addon-actions'
+import MockApolloProvider from '../../../utils/stories/mock-apollo-client'
+import UpdateListTitleButton from './update-list-title-button'
+import { GET_MY_LISTS, UPDATE_LIST } from './update-list-title-button'
+
+const mocks = [
+  {
+    request: {
+      query: GET_MY_LISTS,
+      variables: {
+        id_is: '1',
+      },
+    },
+    result: {
+      data: {
+        updateList: { id: '1', title: 'New Title', __typename: 'List' },
+      },
+    },
+  },
+  {
+    request: {
+      query: UPDATE_LIST,
+      variables: { listId: '1', title: 'New Title' },
+    },
+    result: {
+      data: {
+        updateList: { id: '1', title: 'New Title', __typename: 'List' },
+      },
+    },
+  },
+]
+
+export default {
+  component: UpdateListTitleButton,
+  title: 'UpdateListTitleButton',
+  argTypes: {},
+  decorators: [
+    (story) => <MockApolloProvider mocks={mocks}>{story()}</MockApolloProvider>,
+  ],
+  excludeStories: /.*Data$/,
+}
+
+const buttonData = {
+  listId: '1',
+  newTitle: 'New Title',
+  titleNotUpdated: true,
+}
+
+const actionData = {
+  setTitleNotUpdated: action('update title'),
+  show: action('show alert'),
+}
+
+export const Default = () => <UpdateListTitleButton {...buttonData} />
+
+export const ActiveButton = () => (
+  <UpdateListTitleButton
+    {...{ ...buttonData, titleNotUpdated: false }}
+    {...actionData}
+  />
+)
