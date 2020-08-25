6b737113ba337276ef277c1fcb24fe916f292d51
Brian Andrews <sbardian@gmail.com>
2020-08-25 11:26:37 -0600

    test: add custom renderer

---
+++ src/utils/test/custom-renderer.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import { ThemeProvider } from 'theme-ui'
+import { AlertProvider } from 'react-alerts-plus'
+import { MenuProvider } from '../../components/contexts/menu-context/menu-context'
+import theme from '../../theme'
+
+const AllTheProviders = ({ children }) => {
+  return (
+    <ThemeProvider theme={theme}>
+      <MenuProvider>
+        <AlertProvider>{children}</AlertProvider>
+      </MenuProvider>
+    </ThemeProvider>
+  )
+}
+
+const customRender = (ui, options) =>
+  render(ui, { wrapper: AllTheProviders, ...options })
+
+// re-export everything
+export * from '@testing-library/react'
+
+// override render method
+export { customRender as render }
