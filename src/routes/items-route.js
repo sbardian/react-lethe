6a242362358923143e0b93e1dde55be40e28cfca
Brian Andrews <sbardian@gmail.com>
2020-09-13 15:08:00 -0600

    refactor: remove alert wrapper

--- src/routes/items-route.js
+++ src/routes/items-route.js
@@ -4,7 +4,6 @@ import React from 'react'
 import { jsx } from 'theme-ui'
 import { useParams } from 'react-router-dom'
 import { MdAddCircle } from 'react-icons/md'
-import { AlertWrapper } from 'react-alerts-plus'
 import PageLayout from '../components/page-layout/page-layout'
 import { MenuContext } from '../components/contexts/menu-context/menu-context'
 import Items from '../components/items/items'
@@ -29,67 +28,57 @@ const ListItemsRoute = () => {
 
   return (
     <PageLayout>
-      <AlertWrapper>
-        {({ show }) => (
-          <React.Fragment>
-            <div
+      <div
+        sx={{
+          display: 'grid',
+          gridTemplateRows: '85px 1fr',
+          color: 'textSecondary',
+          height: '100%',
+          padding: 3,
+        }}
+      >
+        <div
+          sx={{
+            display: 'grid',
+            gap: 3,
+            gridTemplateColumns: '1fr 40px',
+          }}
+        >
+          {showListItemTabs && <TabMenu />}
+          <div
+            sx={{
+              display: 'grid',
+              justifySelf: 'end',
+              alignContent: 'end',
+              gap: 3,
+              gridTemplateColumns: '30px 30px',
+              color: 'textSecondary',
+              paddingBottom: 2,
+            }}
+          >
+            <ListSettingsButton listId={listId} hoverColor="colorThree" />
+            <MdAddCircle
+              size="30"
+              role="button"
+              tabIndex={0}
+              onClick={() => setShowDialog(!showDialog)}
               sx={{
-                display: 'grid',
-                gridTemplateRows: '85px 1fr',
-                color: 'textSecondary',
-                height: '100%',
-                padding: 3,
+                cursor: 'pointer',
+                justifySelf: 'end',
+                alignSelf: 'center',
+                color: 'inherit',
+                '&:hover': {
+                  color: 'colorThree',
+                },
               }}
-            >
-              <div
-                sx={{
-                  display: 'grid',
-                  gap: 3,
-                  gridTemplateColumns: '1fr 40px',
-                }}
-              >
-                {showListItemTabs && <TabMenu />}
-                <div
-                  sx={{
-                    display: 'grid',
-                    justifySelf: 'end',
-                    alignContent: 'end',
-                    gap: 3,
-                    gridTemplateColumns: '30px 30px',
-                    color: 'textSecondary',
-                    paddingBottom: 2,
-                  }}
-                >
-                  <ListSettingsButton listId={listId} hoverColor="colorThree" />
-                  <MdAddCircle
-                    size="30"
-                    role="button"
-                    tabIndex={0}
-                    onClick={() => setShowDialog(!showDialog)}
-                    sx={{
-                      cursor: 'pointer',
-                      justifySelf: 'end',
-                      alignSelf: 'center',
-                      color: 'inherit',
-                      '&:hover': {
-                        color: 'colorThree',
-                      },
-                    }}
-                  />
-                </div>
-              </div>
-              <Items listId={listId} show={show} />
-            </div>
-            <Dialog showDialog={showDialog}>
-              <AddItemDialog
-                setShowDialog={setShowDialog}
-                listId={listId}
-                show={show}
-              />
-            </Dialog>
-          </React.Fragment>
-        )}
-      </AlertWrapper>
+            />
+          </div>
+        </div>
+        <Items listId={listId} />
+      </div>
+      <Dialog showDialog={showDialog}>
+        <AddItemDialog setShowDialog={setShowDialog} listId={listId} />
+      </Dialog>
     </PageLayout>
   )
 }
