55d275f81c1a098f8bc42264b23bda6d7648311f
Brian Andrews <sbardian@gmail.com>
2020-09-13 15:09:16 -0600

    refactor: remove alert wrapper

--- src/routes/lists-route.js
+++ src/routes/lists-route.js
@@ -3,7 +3,6 @@
 import React from 'react'
 import { jsx } from 'theme-ui'
 import { RiPlayListAddLine } from 'react-icons/ri'
-import { AlertWrapper } from 'react-alerts-plus'
 import PageLayout from '../components/page-layout/page-layout'
 import ListsContainer from '../components/lists-container/lists-container'
 import AddListDialog from '../components/dialogs/add-list-dialog/add-list-dialog'
@@ -15,52 +14,46 @@ const ListsRoute = () => {
 
   return (
     <PageLayout>
-      <AlertWrapper>
-        {({ show }) => (
-          <React.Fragment>
-            <div
-              sx={{
-                display: 'grid',
-                gridTemplateRows: '40px 1fr',
-                color: 'textDark',
-                padding: 3,
-                height: '100%',
-                width: '100%',
-              }}
-            >
-              <div
-                role="button"
-                tabIndex={0}
-                aria-pressed="false"
-                aria-label="add list"
-                sx={{
-                  display: 'grid',
-                  justifySelf: 'end',
-                  alignContent: 'end',
-                  color: 'textSecondary',
-                  cursor: 'pointer',
-                  paddingBottom: 1,
-                  '&:hover': {
-                    color: 'colorThree',
-                  },
-                }}
-                onKeyPress={(event) => {
-                  handleKeyPress(event, () => setShowDialog(!showDialog))
-                }}
-                onClick={() => {
-                  setShowDialog(!showDialog)
-                }}
-              >
-                <RiPlayListAddLine size="34" />
-              </div>
-              <ListsContainer show={show} />
-            </div>
-            <Dialog showDialog={showDialog}>
-              <AddListDialog setShowDialog={setShowDialog} show={show} />
-            </Dialog>
-          </React.Fragment>
-        )}
-      </AlertWrapper>
+      <div
+        sx={{
+          display: 'grid',
+          gridTemplateRows: '40px 1fr',
+          color: 'textDark',
+          padding: 3,
+          height: '100%',
+          width: '100%',
+        }}
+      >
+        <div
+          role="button"
+          tabIndex={0}
+          aria-pressed="false"
+          aria-label="add list"
+          sx={{
+            display: 'grid',
+            justifySelf: 'end',
+            alignContent: 'end',
+            color: 'textSecondary',
+            cursor: 'pointer',
+            paddingBottom: 1,
+            '&:hover': {
+              color: 'colorThree',
+            },
+          }}
+          onKeyPress={(event) => {
+            handleKeyPress(event, () => setShowDialog(!showDialog))
+          }}
+          onClick={() => {
+            setShowDialog(!showDialog)
+          }}
+        >
+          <RiPlayListAddLine size="34" />
+        </div>
+        <ListsContainer />
+      </div>
+      <Dialog showDialog={showDialog}>
+        <AddListDialog setShowDialog={setShowDialog} />
+      </Dialog>
     </PageLayout>
   )
 }
