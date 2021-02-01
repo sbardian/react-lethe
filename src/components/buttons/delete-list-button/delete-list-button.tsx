8c6e6d7f60823a2f0c169ccf0f60062b25cb9c95
Brian Andrews <sbardian@gmail.com>
2021-01-31 21:41:47 -0700

    refactor: add todo for lists not updating on delete

--- src/components/buttons/delete-list-button/delete-list-button.tsx
+++ src/components/buttons/delete-list-button/delete-list-button.tsx
@@ -10,29 +10,30 @@ interface DeleteListButtonProps {
   listId: string
 }
 
+// TODO: lists not updating after deleting list
 const DeleteListButton: React.FC<DeleteListButtonProps> = ({
   onDeleteList,
   listId,
 }) => (
-    <button
-      data-testid="delete-list-button"
-      aria-label="delete list"
-      tabIndex={0}
-      type="button"
-      onClick={() => onDeleteList(listId)}
-      sx={{
-        border: 'none',
-        backgroundColor: 'transparent',
-        display: 'grid',
-        color: 'inherit',
-        cursor: 'pointer',
-        // justifySelf: 'end',
-        alignSelf: 'center',
-      }}
-    >
-      <FiDelete size="30" />
-    </button>
-  )
+  <button
+    data-testid="delete-list-button"
+    aria-label="delete list"
+    tabIndex={0}
+    type="button"
+    onClick={() => onDeleteList(listId)}
+    sx={{
+      border: 'none',
+      backgroundColor: 'transparent',
+      display: 'grid',
+      color: 'inherit',
+      cursor: 'pointer',
+      // justifySelf: 'end',
+      alignSelf: 'center',
+    }}
+  >
+    <FiDelete size="30" />
+  </button>
+)
 
 DeleteListButton.propTypes = {
   onDeleteList: PropTypes.func.isRequired,
